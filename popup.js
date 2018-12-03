82989c311832bd32e7f3a21711260a3ce8ab25c6
rouxpz <roopa.vasudevan@gmail.com>
2018-12-03 12:23:00 -0500

    linked caman with browser data

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let updatePhoto = document.getElementById("updatePhoto");
+var links = 0;
 
 updatePhoto.onclick = function() {
   chrome.runtime.sendMessage({
@@ -13,7 +14,24 @@ chrome.runtime.onMessage.addListener(
             chrome.extension.getBackgroundPage().console.log("SENT TO POPUP");
             chrome.extension.getBackgroundPage().console.log(request.data);
 
-            document.getElementById("linkNumber").innerHTML = request.data.length + "links collected";
+            links = request.data.length * 10;
+            document.getElementById("linkNumber").innerHTML = request.data.length + " links collected";
+            updateImage();
         }
     }
 );
+
+function updateImage() {
+  Caman("#canvas", function() {
+    //manipulate image here based on data from popup.js
+    // this.crop(200, 300);
+    this.brightness(links);
+    // this.render();
+    // this.resize({
+    //   width: 300,
+    //   height: 389
+    // });
+    this.revert(false);
+    this.render();
+  });
+}
