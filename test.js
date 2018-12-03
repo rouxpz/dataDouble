336715991ccf830ad22a7e851a2723b644b76aa1
rouxpz <roopa.vasudevan@gmail.com>
2018-12-03 12:10:49 -0500

    adding caman js

--- test.js
+++ test.js
@@ -3,20 +3,35 @@ function trackMouse() {
   let links = document.getElementsByTagName("a");
   // console.log(images);
 
-    for (var i = 0; i < images.length-1; i++) {
-      images[i].onmouseover = function() {
-        console.log(this.src);
-      }
-    }
-
-    for (var i = 0; i < links.length; i++) {
-      links[i].onmousedown = function() {
-        console.log(this.href);
-        chrome.runtime.sendMessage({cmd: 'newLink', url: this.href}, function(response) {
-          console.log("link added");
-        });
-      }
+  for (var i = 0; i < images.length-1; i++) {
+    images[i].onmouseover = function() {
+      console.log(this.src);
     }
   }
 
+  for (var i = 0; i < links.length; i++) {
+    links[i].onmousedown = function() {
+      console.log(this.href);
+      chrome.runtime.sendMessage({cmd: 'newLink', url: this.href}, function(response) {
+        console.log("link added");
+      });
+    }
+  }
+}
+
+function trackKeys() {
+  let body = document.body;
+  let line = '';
+
+  body.onkeyup = function(e) {
+    line += e.key;
+    if (e.key === 'Enter') {
+      console.log(line);
+      line = ''
+    }
+
+  }
+}
+
 trackMouse();
+trackKeys();
